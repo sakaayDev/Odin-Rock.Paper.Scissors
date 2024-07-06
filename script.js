2e6b566a3673222648d2b7e7da3b05a87d60670c
sakaayDev <sakaayhakim02@gmail.com>
2024-07-06 07:05:33 +0100

    just writing what i think i have to do in the html using list

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let log = console.log
+log("finally really");
+log("Hello ME and You!");
+// i wrote some kind of pseudocode will need to learn more now i'll follow the odinproject r,p,s page
 
+function getComputerChoice("Rock","Paper","scissors"){
+let choice ="";
+typeof(choice)
+};
+log(Math.random());
+let test =[1,2,3];// i want a function that choose 1 of the 3 hmmm
+// log(test.random()); nice try dumbas lol
