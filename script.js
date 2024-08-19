680f05de302bb3f4a2e5bc2c302c676526d31b87
sakaayDev <sakaayhakim02@gmail.com>
2024-08-19 08:11:19 +0100

    Working button for Rock,Paper,Scissors

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const log = console.log
-log("Hello type log(rpsGame()); to play rock,paper,scissors");
-function getPlayerChoice(){
-    let playerChoice;
-    while (playerChoice != "rock" && playerChoice !="paper" && playerChoice !="scissors" ){
-        playerChoice = prompt(`choose between rock,paper and scissors: `,"what will it be ?").toLowerCase();
-        // i spent like an hour trying || and understanding why it was a infinite loop XD
-    }
-    return playerChoice
-}
+const log = console.log;
 
-// i did if statement first but found it too big and remember that switch exist 
+
+
+
 function getComputerChoice(){
    let choice = Math.floor(Math.random()*3);
     switch(choice) {
@@ -24,42 +17,84 @@ function getComputerChoice(){
             break;
     }return choice;
 };
-// i'm not really sure if this is correct i know there could be a way 
-// to do computer=[] and choose something from it
-function rpsGame(){
+
+function singleRound(humanChoice){
+    let playerScor=0;
+    let computerScor=0;
     let round = 0;
-    let playercount=0;
-    let computercount=0;
-    // while (playercount <3 && computercount <3){ 
-    // by removing condition,only 1 oundis played
-        round++;
-        log("Round"+round);
-        let player = getPlayerChoice();
-        let computer =getComputerChoice();
-        if(player == computer){
+    round++;
+    log("Round"+round);
+    let player = humanChoice;
+    let computer =getComputerChoice();
+    if(player == computer){
+        log("computer chooses "+ computer+", you choose "+ player)
+        log("Draw");
+        log("");
+    }else if(player =="rock" && computer == "scissors" ||
+            player =="paper" && computer == "rock"  ||
+            player =="scissors" && computer == "paper"){
+            playerScor++;
             log("computer chooses "+ computer+", you choose "+ player)
-            log("Draw");
+            log("you win");
+            log("round won by player = "+ playerScor)
             log("");
-        }else if(player =="rock" && computer == "scissors" ||
-                player =="paper" && computer == "rock"  ||
-                player =="scissors" && computer == "paper"){
-                playercount++;
-                log("computer chooses "+ computer+", you choose "+ player)
-                log("you win");
-                log("round won by player = "+ playercount)
-                log("");
-        }else{
-            computercount++;
-            log("computer chooses "+ computer+", you choose "+ player)
-            log("computer wins");
-            log("rounds won by computer = "+ computercount)
-            log("");
-        }
-    // }
-        if(playercount> computercount){
-            return ("player wins "+ playercount+" to " + computercount);
-        }else{
-            return ("Computer wins "+ computercount+" to " + playercount);
-        }
+    }else{
+        computerScor++;
+        log("computer chooses "+ computer+", you choose "+ player)
+        log("computer wins");
+        log("rounds won by computer = "+ computerScor)
+        log("");
     }
-// log(rpsGame(player,computer));
+    }
+
+/*function rpsGame(){
+round++;
+log("Round"+round);
+let player = getPlayerChoice();
+let computer =getComputerChoice();
+if(player == computer){
+    log("computer chooses "+ computer+", you choose "+ player)
+    log("Draw");
+    log("");
+}else if(player =="rock" && computer == "scissors" ||
+        player =="paper" && computer == "rock"  ||
+        player =="scissors" && computer == "paper"){
+        playerScor++;
+        log("computer chooses "+ computer+", you choose "+ player)
+        log("you win");
+        log("round won by player = "+ playerScor)
+        log("");
+}else{
+    computerScor++;
+    log("computer chooses "+ computer+", you choose "+ player)
+    log("computer wins");
+    log("rounds won by computer = "+ computerScor)
+    log("");
+}
+};
+if(playerScor> computerScor){
+    return ("player wins "+ playerScor+" to " + computerScor);
+}else{
+    return ("Computer wins "+ computerScor+" to " + playerScor);
+} */
+
+/* let playGame =document.querySelector("#rps");
+ playGame.addEventListener("click",rpsGame); */
+
+const btnRock =document.querySelector("#rock");
+btnRock.addEventListener("click",function(){
+    let choice="rock";
+    singleRound(choice)
+});
+
+const btnPaper =document.querySelector("#paper");
+btnPaper.addEventListener("click",function(){
+    let choice="paper";
+    singleRound(choice)
+});
+
+const btnScissors =document.querySelector("#scissors");
+btnScissors.addEventListener("click",function(){
+    let choice="scissors";
+    singleRound(choice)
+});
