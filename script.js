e0067f3d623310a2eab15b7ed7a2abf9a16e105c
sakaayDev <sakaayhakim02@gmail.com>
2024-08-28 11:58:58 +0100

    Finished basic Rps in js,with no css

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const log = console.log;
+let gameOver =false;
 let playerScor= 0;
 let computerScor= 0;
 let round = 0;
-
-
+function checkGameOver(){
+    if(playerScor>computerScor ){
+        gameWinner.textContent="Player win,please reset if you want to play again!"
+        gameOver=true;
+    }else {
+        gameWinner.textContent="Computer win,please reset if you want to try again!"
+        gameOver=true;
+    };
+};
 
 function getComputerChoice(){
    let choice = Math.floor(Math.random()*3);
@@ -21,36 +29,49 @@ function getComputerChoice(){
 };
 
 function singleRound(humanChoice){
-    if(playerScor >1){
-        let pTime= "times"
-    }else{pTime ="time"};
-    if(computerScor >1){
-        let cTime= "times"
-    }else{cTime ="time"};
-    round++;
-    pRound =`round ${round}`;
-    let player = humanChoice;
-    let computer =getComputerChoice();
-    if(player == computer){
-        pChoicesMade.textContent =`computer chooses ${computer}, player chooses ${player}`;
-        pRoundWinner.textContent =`it's a DRAW!`;
-
-    }else if(
-            player =="rock" && computer == "scissors" ||
-            player =="paper" && computer == "rock"  ||
-            player =="scissors" && computer == "paper"){
-            playerScor++;
+    if(!gameOver){
+        let pTime="";
+        let cTime="";
+        round++;
+        pRound =`round ${round}`;
+        let player = humanChoice;
+        let computer =getComputerChoice();
+        if(player == computer){
             pChoicesMade.textContent =`computer chooses ${computer}, player chooses ${player}`;
-            pRoundWinner.textContent =`player wins this round!`;
-            pRoundWon.textContent = `player wins ${playerScor} ${pTime},computer wins ${computerScor} ${cTime}`;
-    }else{
+            pRoundWinner.textContent =`it's a DRAW!`;
 
-        computerScor++;
-        pChoicesMade.textContent =`computer chooses ${computer}, player chooses ${player}`;
-        pRoundWinner.textContent = `computer wins this round!`;
-        pRoundWon.textContent = `player wins ${playerScor} ${pTime},computer wins ${computerScor} ${cTime}`;
-    }
-    }
+        }else if(
+                player =="rock"     && computer == "scissors" ||
+                player =="paper"    && computer == "rock"     ||
+                player =="scissors" && computer == "paper"){
+                playerScor++;
+                if(playerScor >1){
+                    pTime= "times";
+                }else{pTime ="time"};
+                if(computerScor >1){
+                    cTime= "times";
+                }else{cTime ="time"};
+                pChoicesMade.textContent =`computer chooses ${computer}, player chooses ${player}`;
+                pRoundWinner.textContent =`player wins this round!`;
+                pRoundWon.textContent    = `player wins ${playerScor} ${pTime},computer wins ${computerScor} ${cTime}`;
+        }else{
+
+            computerScor++;
+            if(playerScor >1){
+                pTime= "times";
+            }else{pTime ="time"};
+            if(computerScor >1){
+                cTime= "times";
+            }else{cTime ="time"};
+            pChoicesMade.textContent =`computer chooses ${computer}, player chooses ${player}`;
+            pRoundWinner.textContent = `computer wins this round!`;
+            pRoundWon.textContent    = `player wins ${playerScor} ${pTime},computer wins ${computerScor} ${cTime}`;
+        };
+        if(playerScor >4 || computerScor >4){
+            checkGameOver();
+        };
+    };
+};
 
 /*function rpsGame(){
 round++;
@@ -88,9 +109,11 @@ if(playerScor> computerScor){
     computerScor = 0;
     playerScor = 0;
     round = 0;
+    gameOver=false;
     pChoicesMade.textContent =``;
     pRoundWinner.textContent =``;
-    pRoundWon.textContent = ``;
+    pRoundWon.textContent    = ``;
+    gameWinner.textContent="game ON";
  }); 
 
 const btnRock =document.querySelector("#rock");
@@ -121,3 +144,7 @@ let pRoundWinner = document.querySelector("#roundWinner");
 
 
 let pRoundWon = document.querySelector("#roundWon");
+
+let gameWinner = document.querySelector("#gameWinner");
+
+
